fcf7c2553e86fb0399504f21387bbe9cbfb9df3a
sarinalalria <lalrsk01@pfw.edu>
2022-11-28 15:37:44 -0500

    fdfs

--- routes/home.js
+++ routes/home.js
@@ -1,20 +1,16 @@
 const express = require("express");
 const router = express.Router();
 const path = require('path');
+const app = express();
 
-
-router.use(express.static('../HTML'))
+app.use(express.static('../HTML'))
 
 router.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../index.html'));
 });
-  
-router.get("/signup", function (req, res) {
-    res.set({
-        'Access-control-Allow-Origin': '*'
-    });
-    return res.redirect('signup.html');
 
+router.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, '../signup.html'));
 });
 
 module.exports = router;
