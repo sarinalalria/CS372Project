aa33439bdf37f90680b08c22eac26d0a215fa604
sarinalalria <lalrsk01@pfw.edu>
2022-11-28 15:17:49 -0500

    jhkj

--- routes/home.js
+++ routes/home.js
@@ -2,13 +2,19 @@ const express = require("express");
 const router = express.Router();
 const path = require('path');
 
-router.use(express.static('../HTML'))
+
+router.use('/public',express.static(__dirname +"/HTML"));
 
 router.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../index.html'));
 });
   
-router.get('/signup', function(req, res) {
-    res.sendFile(path.join(__dirname, '../HTML/signup.html'));
+app.get("/signup", function (req, res) {
+    res.set({
+        'Access-control-Allow-Origin': '*'
+    });
+    return res.redirect('signup.html');
+
 });
+
 module.exports = router;
