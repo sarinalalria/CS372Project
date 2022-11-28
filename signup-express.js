0867c708ce3c9a345cb291c06c5226b5ca912a0d
sarinalalria <lalrsk01@pfw.edu>
2022-11-28 15:02:18 -0500

    fd

--- signup-express.js
+++ signup-express.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
-
+const path = require('path');
 const dotenv = require('dotenv');
 dotenv.config({ path: './.env' });
 
