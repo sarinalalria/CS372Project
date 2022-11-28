c794ef2f9f19d3579df85c5ca736eda03dbe30c2
sarinalalria <lalrsk01@pfw.edu>
2022-11-28 14:33:45 -0500

    cange

--- routes/home.js
+++ routes/home.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const router = express.Router();
-
-
-
 const path = require('path');
 
+router.use(express.static('./HTML'))
 router.get('/', function(req, res) {
     res.sendFile(path.join(__dirname, '../index.html'));
 });
